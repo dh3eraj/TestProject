250915f2d1d05ddadc021220d312f2ef7e17fe82
dh3eraj <73510099+dh3eraj@users.noreply.github.com>
2020-11-24 08:33:21 +0530

    Create DoublePopulation.py
    It is a Python program which tells the time when after which the population will be double.
    It requires current population and growth rate.

---
+++ DoublePopulation.py
@@ -0,0 +1,11 @@
+def dobpop():
+    time=0
+    pop=int(input("Enter Population."))
+    grate=float(input("Enter groth rate."))
+    pop2=pop*2
+    while pop<=pop2:
+          pop=pop+pop*grate
+          time=time+1
+    print("Population:",pop.round())
+    print("Time:",time,"min.")
+dobpop()
